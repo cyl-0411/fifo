e7f2cd896dfbe4325e6e26180b8f9f6a2660bfe8
cyl0411 <546750558@qq.com>
2023-10-23 21:29:24 +0800

    finish gray->bin

--- src/main/scala/gcd/GCD.scala
+++ src/main/scala/gcd/GCD.scala
@@ -5,7 +5,7 @@ import chisel3.util.Decoupled
 import chisel3.util.Queue
 
 
-class fifo[T <: Data] (data:T,depth:Int) extends RawModule {
+class fifo[T <: Data] (data:T,depth:Int) extends Module {
   val io = IO(new Bundle {
     val write        = Flipped(Decoupled(data))
     val rst_w        = Input(Bool())
@@ -16,15 +16,70 @@ class fifo[T <: Data] (data:T,depth:Int) extends RawModule {
     val clk_r        = Input(Clock())
   })
 
-  withClockAndReset ( io.clk_r , io.rst_r ) {
+  val mem = Mem(depth,data)
+  val rd_ptr = RegInit(0.U)
+  val wr_ptr = RegInit(0.U)
+  val wr_full = RegInit(false.B)
+  val rd_empty = RegInit(true.B)
+  val rd_en = RegInit(false.B)
+  val wr_en = RegInit(false.B)
 
+/*rd_ptr is increasing*/
+  withClockAndReset ( io.clk_r , io.rst_r ) {
+    when (io.rst_r === true.B) {
+      rd_ptr := 0.U
+    }
+
+    when( rd_ptr < wr_ptr && rd_empty ===false.B && rd_en === true.B) {
+      rd_ptr := rd_ptr +1.U 
+    } .otherwise {
+      rd_ptr := rd_ptr
+    }
   }
 
+  /*  write_ptr   */
   withClockAndReset ( io.clk_w , io.rst_w ) {
     
   }
-
+  io.write <> io.read
 }
+
+/* rd_empty generate */
+
+/* wr_full generate */
+
+class BinToGray (width: Int) extends Module {
+  val io = IO(new Bundle {
+    val bin     = Input(UInt(width.W))
+    val gray    = Output(UInt(width.W))
+  })
+
+    io.gray := io.bin ^ (io.bin >> 1.U)
+}
+
+class GrayToBin (width: Int) extends Module {
+  val io = IO(new Bundle {
+    val bin     = Output(UInt(width.W))
+    val gray    = Input(UInt(width.W))
+  })
+    val binary = Wire(Vec(width,UInt(1.W)))
+    binary(width-1) := io.gray(width-1)
+    for (i <- 2 until (width+1)) {
+      binary(width-i) := io.gray(width-i) ^ binary(width-i+1) 
+    }
+    io.bin := binary.asUInt() 
+}
+
+
+
+// object Convert{
+//   def BintoGray(bin:UInt):UInt = {
+
+//   }
+//   def GraytoBin(gray:UInt):UInt = {
+
+//   }
+// }
 
 object Elaborate extends App {
   (new chisel3.stage.ChiselStage).emitVerilog(new fifo(UInt(32.W),4))
--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -10,19 +10,37 @@ import fifo._
 
 class fifotest extends AnyFreeSpec with ChiselScalatestTester {
 
-  "FIFO should pass" in {
-    test(new fifo()) { dut =>
-      dut.io.value1.poke(1.U)
-      dut.io.value2.poke(1.U)
-      dut.io.value3.expect(1.U)
+  "Module “BinToGray” should pass" in {
+    test(new BinToGray(8)) { dut =>
+      dut.io.bin.poke("b10101010".U)
+      dut.io.gray.expect("b11111111".U)
 
-      dut.io.value1.poke(0.U)
-      dut.io.value2.poke(1.U)
-      dut.io.value3.expect(0.U)
-            
-      dut.io.value1.poke(1.U)
-      dut.io.value2.poke(0.U)
-      dut.io.value3.expect(0.U)      
+      dut.io.bin.poke("b00000000".U)
+      dut.io.gray.expect("b00000000".U)
+
+      dut.io.bin.poke("b01010101".U)
+      dut.io.gray.expect("b01111111".U)
+
+      dut.io.bin.poke("b11001100".U)
+      dut.io.gray.expect("b10101010".U)
+      
+    }
+  }
+
+    "Module “GrayToBin” should pass" in {
+    test(new GrayToBin(8)) { dut =>
+                 
+      dut.io.gray.poke("b00000000".U) 
+      dut.io.bin.expect("b00000000".U)
+
+      dut.io.gray.poke("b10101010".U)
+      dut.io.bin.expect("b11001100".U) 
+
+      dut.io.gray.poke("b01111111".U)
+      dut.io.bin.expect("b01010101".U)
+      
+      dut.io.gray.poke("b11111111".U) 
+      dut.io.bin.expect("b10101010".U)
     }
   }
 }
