ca80ef0e6aff71063c0c3963d877e9386fbc1fbc
cyl0411 <546750558@qq.com>
2023-11-26 17:08:36 +0800

    before test

--- src/main/scala/gcd/fifo.scala
+++ src/main/scala/gcd/fifo.scala
@@ -21,11 +21,15 @@ class fifo[T <: Data] (data:T,depth:Int) extends Module {
   val wr_en = io.write.valid && io.write.ready
   val rd_en = io.read.valid && io.read.ready
   val ptr_width = log2Ceil(depth) + 1
+
   val rd_ptr = RegInit(0.U(ptr_width.W))
   val wr_ptr = RegInit(0.U(ptr_width.W))
+  val wr_ptr_r = ptr_converter(wr_ptr,io.clk_r)
+  val rd_ptr_w = ptr_converter(rd_ptr,io.clk_w)
 
-  val rd_empty = rd_ptr === wr_ptr
-  val wr_full = (wr_ptr.head(1) =/= rd_ptr.head(1)) && (wr_ptr.tail(1) === rd_ptr.tail(1))
+
+  val rd_empty = rd_ptr === wr_ptr_r
+  val wr_full = (wr_ptr.head(1) =/= rd_ptr_w.head(1)) && (wr_ptr.tail(1) === rd_ptr_w.tail(1))
   io.read.valid := !rd_empty
   io.write.ready:= !wr_full
 
@@ -37,12 +41,30 @@ class fifo[T <: Data] (data:T,depth:Int) extends Module {
       io.read.bits := Mux(rd_en, ram.read(rd_ptr,io.clk_r),0.U.asTypeOf(data))
       rd_ptr := rd_ptr + 1.U 
     }
-      val wr_ptr_1 = RegNext(wr_ptr)
-      val wr_ptr_r = RegNext(wr_ptr_1)
 
   }
-}
 
+  withClockAndReset( io.clk_w , io.rst_w){
+    when(io.rst_w === false.B){
+      wr_ptr := 0.U
+    } .otherwise{
+      when( wr_en ) {
+        ram.write(wr_ptr , io.write.bits , io.clk_w)
+        wr_ptr := wr_ptr + 1.U
+      }
+    }
+  }
+}
+object ptr_converter {
+  def apply(ptr : UInt , clk : Clock) : UInt = {
+    withClock(clk){
+      val ptr_1 = RegNext(ptr)
+      val ptr_2 = RegNext(ptr_1)
+      ptr_2 := ptr_1
+      ptr_2
+    }
+  }
+}
 object BinToGray {
   def apply(bin : UInt , width : Int) : UInt = {
     val gray = Wire(UInt(32.W))
